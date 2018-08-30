b97703f17bc74250938c96f9048b219a21087ab1
Artur Nowak <artur.nowak@xsolve.pl>
2018-08-30 11:31:54 +0200

    Fixed api envs

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -125,6 +125,7 @@ module.exports = options => ({
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_ENV: JSON.stringify(process.env.API_ENV),
       },
     }),
   ]),
