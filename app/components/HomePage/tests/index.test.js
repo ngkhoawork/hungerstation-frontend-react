6cb8a66ae6eaf4bd6c45194e8479d76aa9c83ae0
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-09-12 13:21:52 +0200

    Adding unit tests from ResetPassword page

--- app/components/HomePage/tests/index.test.js
+++ app/components/HomePage/tests/index.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import HomePage from '../index';
 import StyledPage from '../StyledPage';
@@ -14,7 +13,6 @@ describe('<HomePage />', () => {
 
   describe('HomePage children components', () => {
     let wrapper;
-    Enzyme.configure({ adapter: new Adapter() });
     beforeEach(() => {
       wrapper = shallow(<HomePage />);
     });
--- app/components/ResetPasswordPage/tests/index.test.js
+++ app/components/ResetPasswordPage/tests/index.test.js
@@ -1,10 +1,38 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import ResetPasswordPage from '../index';
+import StyledPage from 'components/StyledFormPage';
+import ResetPasswordPage from '../index';
+
+const renderResetPasswordPage = () => shallow(<ResetPasswordPage />);
 
 describe('<ResetPasswordPage />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('It should render without crashing', () => {
+    renderResetPasswordPage();
+  });
+
+  describe('ResetPasswordPage children components', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<ResetPasswordPage />);
+    });
+
+    it('it should render StyledPage component', () => {
+      expect(wrapper.find(StyledPage)).toHaveLength(1);
+    });
+
+    it('it should render TextItem components', () => {
+      expect(wrapper.find('TextItem')).toHaveLength(2);
+    });
+
+    it('it should render SocialAuth components', () => {
+      expect(wrapper.find('SocialAuth')).toHaveLength(1);
+    });
+
+    it('it should render ResetPasswordFormContainer components', () => {
+      expect(
+        wrapper.find('withRouter(WithStyles(Connect(Form)))'),
+      ).toHaveLength(1);
+    });
   });
 });
