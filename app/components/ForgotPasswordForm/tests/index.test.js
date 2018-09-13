a80cd4ceb64f484f6519dc4d17e54c35ac8eb320
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-09-13 10:00:57 +0200

    Adding saga tests for Login Container

--- app/components/ForgotPasswordForm/tests/index.test.js
+++ app/components/ForgotPasswordForm/tests/index.test.js
@@ -1,10 +1,32 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import ForgotPasswordForm from '../index';
+import StyledForm from 'components/StyledForm';
+import ForgotPasswordForm from '../index';
+
+const props = {
+  handleSubmit: jest.fn(),
+  submitting: false,
+  classes: {
+    button: 'btn',
+  },
+};
+const renderForgotPasswordForm = () =>
+  shallow(<ForgotPasswordForm {...props} />);
 
 describe('<ForgotPasswordForm />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('It should render without crashing', () => {
+    renderForgotPasswordForm();
+  });
+
+  describe('ForgotPasswordForm children components', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<ForgotPasswordForm {...props} />);
+    });
+
+    it('it should render StyledForm component', () => {
+      expect(wrapper.find(StyledForm)).toHaveLength(1);
+    });
   });
 });
--- app/components/RegistrationPage/tests/index.test.js
+++ app/components/RegistrationPage/tests/index.test.js
@@ -1,10 +1,38 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import RegistrationPage from '../index';
+import StyledPage from 'components/StyledFormPage';
+import RegistrationPage from '../index';
+
+const renderRegistrationPage = () => shallow(<RegistrationPage />);
 
 describe('<RegistrationPage />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('It should render without crashing', () => {
+    renderRegistrationPage();
+  });
+
+  describe('RegistrationPage children components', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<RegistrationPage />);
+    });
+
+    it('it should render StyledPage component', () => {
+      expect(wrapper.find(StyledPage)).toHaveLength(1);
+    });
+
+    it('it should render TextItem components', () => {
+      expect(wrapper.find('TextItem')).toHaveLength(3);
+    });
+
+    it('it should render SocialAuth components', () => {
+      expect(wrapper.find('SocialAuth')).toHaveLength(1);
+    });
+
+    it('it should render RegistrationFormContainer components', () => {
+      expect(
+        wrapper.find('Connect(withRouter(WithStyles(Connect(Form))))'),
+      ).toHaveLength(1);
+    });
   });
 });
