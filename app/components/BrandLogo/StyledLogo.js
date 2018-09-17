9a98c5a4bf84dd34aaed4619b724e3c5b93712fe
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-09-17 14:29:49 +0200

    Fixing code review comments

--- app/components/BrandLogo/StyledLogo.js
+++ app/components/BrandLogo/StyledLogo.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-import StyledBrandDetails from 'components/RestaurantsPage/RestaurantsSection/OffersList/OfferItem/BrandDetails/StyledBrandDetails';
-
 const StyledLogo = styled.img`
   width: 26px;
   height: 26px;
@@ -9,9 +7,6 @@ const StyledLogo = styled.img`
   border-radius: 13px;
   background-color: white;
   margin-right: 20px;
-  ${StyledBrandDetails} && {
-    margin-bottom: 4px;
-  }
 `;
 
 export default StyledLogo;
