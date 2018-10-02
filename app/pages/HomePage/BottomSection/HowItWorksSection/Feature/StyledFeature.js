d73ba2504b40090955a6baaa12bf85a54e7b6b61
Andriy Roybul <andriy.roybul@boldare.com>
2018-10-02 14:11:18 +0200

    Fixed styles issues, changed block to static width, moved icon to absolute position

--- app/pages/HomePage/BottomSection/HowItWorksSection/Feature/StyledFeature.js
+++ app/pages/HomePage/BottomSection/HowItWorksSection/Feature/StyledFeature.js
@@ -1,17 +1,36 @@
 import styled from 'styled-components';
-import { flexBox, mediaMedium } from 'utils/css/styles';
+import { flexBox, mediaMedium, mediaSmall } from 'utils/css/styles';
 
 const StyledFeature = styled.span`
   ${flexBox(
     {},
     `
-    flex: 0.3;
+    width: 224px;
+    max-width: 224px;
+    position: relative;
+    margin-right: 192px;
+
   `,
   )};
   ${mediaMedium`
     flex: 1;
-    margin: 0 35px 0 20px;
+    margin: 0 35px 0px 20px;
+    margin-right: 0px;
   `};
+  ${mediaSmall`
+  margin: 0 0 0 72px;
+  `};
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+export const StyledFeatureIcon = styled.div`
+  position: absolute;
+  top: -6px;
+  left: -68px;
+  z-index: 100;
 `;
 
 export default StyledFeature;
