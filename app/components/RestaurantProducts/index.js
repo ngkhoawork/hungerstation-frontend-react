86b0db513deca296f7ef50b0e2a05f0e4ad4a86b
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-23 01:19:44 +0900

    using new icon

--- app/components/RestaurantProducts/index.js
+++ app/components/RestaurantProducts/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import intl from 'utils/intlService';
 import Icon from 'components/Icon';
+import Price from 'components/Price';
 import {
   List,
   Item,
@@ -11,7 +11,6 @@ import {
   Description,
   Img,
   PriceContainer,
-  Price,
   Footer,
   MobileFooter,
 } from './StyledComponents';
@@ -19,14 +18,11 @@ import {
 const renderFooter = price => (
   <Fragment>
     <PriceContainer>
-      <Icon name="price-tag" />
-      <Price>
-        {intl.formatNumber(price, { style: 'currency', currency: 'SAR' })}
-      </Price>
+      <Price price={price} isPrimary hasTag />
     </PriceContainer>
     <span role="button">
       Add to cart &nbsp;
-      <Icon name="arrow-back" size={15} />
+      <Icon name="arrow-circled-right" size={15} />
     </span>
   </Fragment>
 );
