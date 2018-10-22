40ecc8289346ddcec50cb2edf4bdf112762bc2f0
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-22 22:24:14 +0900

    global css variables introduced

---
+++ app/utils/css/variables.js
@@ -0,0 +1,8 @@
+export const fontFamilyRegular = 'HungerStation-Regular, sans-serif';
+export const boxShadow = '8px 12px 23px -3px rgba(59, 59, 59, 0.13)';
+export const borderRadius = '8px';
+
+export const pageOffsetX = '113px';
+export const mobPageOffsetX = '30px';
+
+export const zIndexDropdown = 700;
