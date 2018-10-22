71c476c2f5687ac6ad21ab9c6d2a64b666ba5a20
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-22 22:25:47 +0900

    global css colors update

--- app/utils/css/colors.js
+++ app/utils/css/colors.js
@@ -1,6 +1,7 @@
 export const fuscousGray = '#434340';
 export const gold = '#FFD700';
 export const persimmon = '#FF5F4F';
+export const errorLight = '#fff1f1';
 export const alto = '#D8D8D8';
 export const ironsideGray = '#6F6E6B';
 export const bermuda = '#7CD8AE';
@@ -8,7 +9,8 @@ export const hintOfRed = '#f8f6f6';
 export const paleSlate = '#cdcbcd';
 export const wildSand = '#f4f4f4';
 export const jade = '#00C269';
-export const silverChalice = '#9F9F9F';
 export const boulder = '#7C7C7C';
 export const dustyGray = '#979797';
+export const silverChalice = '#9F9F9F';
+export const lightGray = '#f0f0f0';
 export const alabaster = '#f7f7f7';
