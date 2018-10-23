42d61bee1cd37f8d312842058232b12a29197730
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-23 09:03:30 +0900

    chevron icon component

---
+++ app/components/ChevronIcon/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Icon from 'components/Icon';
+
+export const IconContainer = styled.span`
+  display: inline-block;
+  position: relative;
+  transform: rotate(${({ isOpen }) => (isOpen ? 180 : 0)}deg);
+  top: ${({ isOpen }) => (isOpen ? -1 : 0)}px;
+`;
+
+const ChevronIcon = props => (
+  <IconContainer {...props}>
+    <Icon name="chevron" />
+  </IconContainer>
+);
+
+ChevronIcon.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+export default ChevronIcon;
