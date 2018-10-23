4a132617ac0e64ea0619eec6fe870c19f1827dd6
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-24 01:13:12 +0900

    quantity select component

---
+++ app/components/QuantitySelect/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { fontCorrection } from 'utils/css/styles';
+import { gold, lightGray } from 'utils/css/colors';
+import { fontFamilyRegular } from 'utils/css/variables';
+import CircledItem from 'components/CircledItem';
+
+const Container = styled.div`
+  display: inline-flex;
+  align-items: center;
+  font-family: ${fontFamilyRegular};
+  line-height: 1;
+  font-size: 18px;
+`;
+
+const Quantity = styled.span`
+  margin: 0 10px;
+  ${fontCorrection};
+`;
+
+const QuantitySelect = ({ quantity, onChange }) => (
+  <Container>
+    <CircledItem
+      color={lightGray}
+      width={24}
+      onClick={() => onChange(quantity - 1)}
+    >
+      <span style={{ zIndex: 1, paddingTop: 3 }}>-</span>
+    </CircledItem>
+    <Quantity>{quantity}</Quantity>
+    <CircledItem color={gold} width={24} onClick={() => onChange(quantity + 1)}>
+      <span style={{ zIndex: 1, paddingTop: 3 }}>+</span>
+    </CircledItem>
+  </Container>
+);
+
+QuantitySelect.propTypes = {
+  quantity: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default QuantitySelect;
