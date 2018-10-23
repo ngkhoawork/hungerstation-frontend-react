615ccda921cd1573bc88fbbe597ab2992bbdf5cb
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-24 04:15:12 +0900

    modal off page scroll fix

--- app/containers/ModalContainer/actions.js
+++ app/containers/ModalContainer/actions.js
@@ -6,10 +6,11 @@
 
 import { SHOW_MODAL, HIDE_MODAL } from './constants';
 
-export const showModal = view => ({
-  type: SHOW_MODAL,
-  payload: { view },
-});
+export const showModal = view => {
+  window.scrollTo(0, 0);
+
+  return { type: SHOW_MODAL, payload: { view } };
+};
 
 export const hideModal = () => ({
   type: HIDE_MODAL,
