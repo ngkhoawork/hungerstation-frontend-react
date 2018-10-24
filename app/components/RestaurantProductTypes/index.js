fe33c35ced7b18f63a5e7a4c6ede82163e943710
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-25 05:10:13 +0900

    product types update

--- app/components/RestaurantProductTypes/index.js
+++ app/components/RestaurantProductTypes/index.js
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types';
 import Icon from 'components/Icon';
 import { ProductTypeBar, ProductType, Name } from './StyledComponents';
 
-const RestaurantProductTypes = ({ types, active }) => (
+const RestaurantProductTypes = ({ types, active, onClick }) => (
   <ProductTypeBar>
     {types.map(type => (
-      <ProductType key={type} active={active === type}>
+      <ProductType
+        key={type}
+        active={active === type}
+        onClick={() => onClick(type)}
+      >
         <Name active={active === type}>{type}</Name>
         {active === type ? <Icon name="check-mark-green" size={18} /> : null}
       </ProductType>
@@ -17,6 +21,7 @@ const RestaurantProductTypes = ({ types, active }) => (
 RestaurantProductTypes.propTypes = {
   types: PropTypes.arrayOf(PropTypes.string).isRequired,
   active: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default RestaurantProductTypes;
