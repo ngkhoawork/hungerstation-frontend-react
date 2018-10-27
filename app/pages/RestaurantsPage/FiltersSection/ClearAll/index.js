d504806b42e30f11c422676f640c069555bb24c7
Andriy Roybul <andriy.roybul@boldare.com>
2018-10-27 17:32:50 +0200

    fixed appearence of clearButton

--- app/pages/RestaurantsPage/FiltersSection/ClearAll/index.js
+++ app/pages/RestaurantsPage/FiltersSection/ClearAll/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { func } from 'prop-types';
+import { connect } from 'react-redux';
+import { func, bool } from 'prop-types';
 import intl from 'utils/intlService';
 import Icon from 'components/Icon';
 import Group from 'components/Group';
 import Paragraph from 'components/Paragraph';
+import { selectIsFiltersInitial } from 'modules/restaurants/selectors';
 import messages from './messages';
 
-const ClearAll = ({ resetFilters }) => (
-  <Group onClick={resetFilters}>
-    <Paragraph size={12} margin="0 5px 0 0">
-      {intl.formatMessage(messages.clearAll)}
-    </Paragraph>
-    <Icon name="delete" />
-  </Group>
-);
+const decorate = connect(state => ({
+  isFiltersInitial: selectIsFiltersInitial(state),
+}));
+
+const ClearAll = ({ resetFilters, isFiltersInitial }) =>
+  !isFiltersInitial && (
+    <Group onClick={resetFilters}>
+      <Paragraph size={12} margin="0 5px 0 0">
+        {intl.formatMessage(messages.clearAll)}
+      </Paragraph>
+      <Icon name="delete" />
+    </Group>
+  );
 
 ClearAll.propTypes = {
   resetFilters: func,
+  isFiltersInitial: bool,
 };
-export default ClearAll;
+export default decorate(ClearAll);
