ee21c41165dbd1f52b28c94a371e45b000048b78
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-30 19:33:19 +0900

    restaurant name selector added

--- app/modules/restaurant/selectors.js
+++ app/modules/restaurant/selectors.js
@@ -1,4 +1,10 @@
+import { createSelector } from 'reselect';
 import { initialState } from './reducer';
 
 export const selectRestaurantState = state =>
   state.get('restaurant', initialState).toJS();
+
+export const selectRestaurantName = createSelector(
+  selectRestaurantState,
+  state => state.restaurant.name,
+);
