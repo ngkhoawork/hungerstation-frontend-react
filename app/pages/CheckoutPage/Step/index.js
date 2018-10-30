2334acac7c11e2d4c9d04e32f73caae2746e19fe
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-30 19:37:52 +0900

    step component implemented

---
+++ app/pages/CheckoutPage/Step/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { flex, borderBottom } from 'utils/css/styles';
+import { stepIndent } from 'utils/css/variables';
+import { fuscousGray, silverChalice } from 'utils/css/colors';
+import intl from 'utils/intlService';
+import { Title } from 'components/Typography';
+import messages from './messages';
+
+const Step = ({ title, stepNo, stepCount, children }) => (
+  <Container>
+    <Header>
+      <Index>
+        <StepNo>{`${intl.formatMessage(messages.step)} ${stepNo}`}</StepNo>
+        <StepCount>
+          {` - ${intl.formatMessage(messages.of)} ${stepCount}`}
+        </StepCount>
+      </Index>
+      <Title>{title}</Title>
+    </Header>
+    <Content>{children}</Content>
+  </Container>
+);
+
+Step.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.element,
+    PropTypes.array,
+  ]).isRequired,
+  title: PropTypes.string.isRequired,
+  stepNo: PropTypes.number.isRequired,
+  stepCount: PropTypes.number.isRequired,
+};
+
+export default Step;
+
+const Container = styled.section`
+  margin-bottom: 30px;
+`;
+
+const Header = styled.div`
+  ${flex({ align: 'center' })};
+  ${borderBottom};
+  padding: 30px 0;
+`;
+
+const Index = styled.span`
+  width: ${stepIndent};
+  font-size: 14px;
+  line-height: 1;
+`;
+
+const StepNo = styled.span`
+  color: ${fuscousGray};
+`;
+
+const StepCount = styled.span`
+  color: ${silverChalice};
+`;
+
+const Content = styled.div`
+  padding: 30px 0 30px ${stepIndent};
+`;
---
+++ app/pages/CheckoutPage/Step/messages.js
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  step: {
+    id: 'app.components.CheckoutPage.Step.step',
+    defaultMessage: 'Step',
+  },
+  of: {
+    id: 'app.components.CheckoutPage.Step.of',
+    defaultMessage: 'of',
+  },
+});
