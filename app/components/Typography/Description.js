5971073a006ad00494859269d6aa70b3d82ed4e3
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-10-30 19:31:10 +0900

    title and description extracted into typography component

---
+++ app/components/Typography/Description.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { ironsideGray } from 'utils/css/colors';
+import { fontFamilyLight } from 'utils/css/variables';
+
+const Description = styled.div`
+  color: ${ironsideGray};
+  font-family: ${fontFamilyLight};
+  font-size: 14px;
+  letter-spacing: 0.44px;
+  line-height: 16px;
+  margin: 10px 0;
+`;
+
+export default Description;
---
+++ app/components/Typography/Title.js
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components';
+import { fuscousGray } from 'utils/css/colors';
+import { fontFamilyRegular } from 'utils/css/variables';
+
+const Title = styled.div`
+  color: ${fuscousGray};
+  font-family: ${fontFamilyRegular};
+  font-size: ${({ fontSize }) => fontSize || 24}px;
+  letter-spacing: ${({ letterSpacing }) => letterSpacing || 0.33}px;
+  line-height: 1;
+
+  ${({ style }) => style && css(style)};
+`;
+
+export default Title;
