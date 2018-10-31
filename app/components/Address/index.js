edcc61b0051005f8a358a398403c23b8f6e1fb95
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-01 03:19:45 +0900

    address component update

--- app/components/Address/index.js
+++ app/components/Address/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import intl from 'utils/intlService';
+import { addressTypesObj, otherAddressType } from 'modules/address/constants';
+import addressMessages from 'modules/address/messages';
 import { flex } from 'utils/css/styles';
 import { fuscousGray, alabaster } from 'utils/css/colors';
 import {
@@ -16,7 +18,28 @@ import Icon from 'components/Icon';
 import CheckboxIcon from 'components/CheckboxIcon';
 import CircledItem from 'components/CircledItem';
 import messages from './messages';
-import { addressTypesObj } from './constants';
+
+const getName = ({ name, specific_type, street, building_number }) => {
+  if (specific_type && specific_type !== otherAddressType) {
+    return intl.formatMessage(addressMessages[specific_type]);
+  }
+
+  return name || `${street} ${building_number || ''}`.trim();
+};
+
+const getDescription = ({
+  specific_type,
+  description,
+  street,
+  building_number,
+  name,
+}) => {
+  const address = `${street} ${building_number || ''}`.trim();
+
+  if (specific_type) return address;
+
+  return name ? address : description;
+};
 
 const Address = ({
   address,
@@ -24,44 +47,64 @@ const Address = ({
   isWithBorder,
   onSelectToggle,
   onEditClick,
-}) => (
-  <Container onClick={onSelectToggle} isWithBorder={isWithBorder}>
-    <LeftSide>
-      <CheckboxIcon isChecked={isSelected} />
-    </LeftSide>
-    <Content>
-      <div>
-        <Title>{intl.formatMessage(messages[address.type])}</Title> &nbsp;
-        <CircledItem color={alabaster} inline width={28}>
-          <Icon name={addressTypesObj[address.type].icon} size={16} />
-        </CircledItem>
-      </div>
-      <Location>{address.address}</Location>
-    </Content>
-    <Button
-      primary={false}
-      color={alabaster}
-      lift={false}
-      inline
-      size="l"
-      onClick={onEditClick}
+}) => {
+  const handleEditBtnClick = event => {
+    event.stopPropagation();
+    onEditClick(address);
+  };
+
+  return (
+    <Container
+      onClick={() => onSelectToggle && onSelectToggle(address)}
+      isWithBorder={isWithBorder}
     >
-      <React.Fragment>
-        {intl.formatMessage(messages.edit)} &nbsp;
-        <Icon name="edit" size={16} />
-      </React.Fragment>
-    </Button>
-  </Container>
-);
+      <LeftSide>
+        <CheckboxIcon isChecked={isSelected} />
+      </LeftSide>
+      <Content>
+        <Name>
+          <Title>{getName(address)}</Title>
+          <CircledItem
+            color={alabaster}
+            inline
+            width={28}
+            style={typeIconStyle}
+          >
+            <Icon
+              name={
+                address.specific_type
+                  ? addressTypesObj[address.specific_type].icon
+                  : 'district-v2'
+              }
+              size={16}
+            />
+          </CircledItem>
+        </Name>
+        <Location>{getDescription(address)}</Location>
+      </Content>
+      <Button
+        primary={false}
+        color={alabaster}
+        lift={false}
+        inline
+        size="l"
+        style={{ whiteSpace: 'nowrap' }}
+        onClick={handleEditBtnClick}
+      >
+        <React.Fragment>
+          {intl.formatMessage(messages.edit)} &nbsp;
+          <Icon name="edit" size={16} />
+        </React.Fragment>
+      </Button>
+    </Container>
+  );
+};
 
 Address.propTypes = {
-  address: PropTypes.shape({
-    type: PropTypes.string.isRequired,
-    address: PropTypes.string.isRequired,
-  }).isRequired,
+  address: PropTypes.object.isRequired,
   isSelected: PropTypes.bool,
   isWithBorder: PropTypes.bool,
-  onSelectToggle: PropTypes.func.isRequired,
+  onSelectToggle: PropTypes.func,
   onEditClick: PropTypes.func.isRequired,
 };
 
@@ -87,7 +130,28 @@ const Content = styled.div`
   line-height: 1;
   color: ${fuscousGray};
   flex-grow: 1;
+  margin-right: 10px;
 `;
+// overflow: hidden;
+
+const Name = styled.div`
+  position: relative;
+`;
+
+// const Name = styled.div`
+//   position: relative;
+//   padding-right: 30px;
+//   line-height: 27px;
+//   overflow: hidden;
+//   white-space: nowrap;
+//   text-overflow: ellipsis;
+// `;
+
+const typeIconStyle = {
+  // position: 'absolute',
+  // right: 0,
+  // top: 0,
+};
 
 const Title = styled.span`
   font-family: ${fontFamilyRegular};
--- app/components/Address/messages.js
+++ app/components/Address/messages.js
@@ -5,24 +5,4 @@ export default defineMessages({
     id: 'app.components.Address.edit',
     defaultMessage: 'Edit address',
   },
-  home: {
-    id: 'app.components.Address.home',
-    defaultMessage: 'Home',
-  },
-  work: {
-    id: 'app.components.Address.work',
-    defaultMessage: 'Work',
-  },
-  hangout: {
-    id: 'app.components.Address.hangout',
-    defaultMessage: 'Hangout',
-  },
-  camp: {
-    id: 'app.components.Address.camp',
-    defaultMessage: 'Camp',
-  },
-  other: {
-    id: 'app.components.Address.other',
-    defaultMessage: 'Other',
-  },
 });
