968d5c7bff223d628e3f4f81d26b7ae02ca3d22a
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-01 03:21:28 +0900

    addresses container update

--- app/containers/AddressesContainer/index.js
+++ app/containers/AddressesContainer/index.js
@@ -1,6 +1,53 @@
-import Loadable from 'react-loadable';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import {
+  selectAddresses,
+  selectPrimaryAddress,
+} from 'modules/address/selectors';
+import { setPrimaryAddress } from 'modules/address/actions';
+import Addresses from 'components/Addresses';
+import AddAddressContainer from 'containers/AddAddressContainer';
+import { showModal } from 'containers/ModalContainer/actions';
 
-export default Loadable({
-  loader: () => import('./hoc'),
-  loading: () => null,
-});
+class AddressesContainer extends React.Component {
+  handleAddAddress = address => {
+    const AddAddressContainerHOC = () => (
+      <AddAddressContainer address={address} />
+    );
+
+    this.props.showModal(AddAddressContainerHOC);
+  };
+
+  handleAddressSelect = address => this.props.setPrimaryAddress(address);
+
+  render() {
+    const { addresses, primaryAddress } = this.props;
+
+    return (
+      <Addresses
+        selectedAddress={primaryAddress}
+        recentAddresses={addresses.filter(addr => addr !== primaryAddress)}
+        onAddClick={this.handleAddAddress}
+        onEditClick={this.handleAddAddress}
+        onSelectToggle={this.handleAddressSelect}
+      />
+    );
+  }
+}
+
+AddressesContainer.propTypes = {
+  primaryAddress: PropTypes.object,
+  addresses: PropTypes.array.isRequired,
+  showModal: PropTypes.func.isRequired,
+  setPrimaryAddress: PropTypes.func.isRequired,
+};
+
+export default connect(
+  createStructuredSelector({
+    addresses: selectAddresses,
+    primaryAddress: selectPrimaryAddress,
+  }),
+  { showModal, setPrimaryAddress },
+)(AddressesContainer);
