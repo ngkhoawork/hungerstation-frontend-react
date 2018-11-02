49a424a4ebfd8dc98a4656bb8c382520335e8de9
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-02 13:50:13 +0900

    note component

---
+++ app/components/Note/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import TextField from '@material-ui/core/TextField';
+import intl from 'utils/intlService';
+import { flex, mediaMedium } from 'utils/css/styles';
+import { stepIndent } from 'utils/css/variables';
+import { Title, Description } from 'components/Typography';
+import messages from './messages';
+
+const Container = styled.div`
+  ${flex({ align: 'center' })};
+
+  ${mediaMedium`${flex({ direction: 'column', align: 'flex-start' })};`};
+
+  ${({ style }) => style && css(style)};
+`;
+
+const Header = styled.div`
+  flex-shrink: 0;
+  width: ${stepIndent};
+`;
+
+const Note = ({ note, style }) => (
+  <Container style={style}>
+    <Header>
+      <Description style={{ margin: 0 }}>
+        {intl.formatMessage(messages.optional)}
+      </Description>
+      <Title>{intl.formatMessage(messages.notes)}</Title>
+    </Header>
+    <TextField
+      type="text"
+      defaultValue={note}
+      label={intl.formatMessage(messages.instructions)}
+      fullWidth
+    />
+  </Container>
+);
+
+Note.propTypes = {
+  note: PropTypes.string,
+  style: PropTypes.object,
+};
+
+export default Note;
---
+++ app/components/Note/messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  notes: {
+    id: 'app.components.Note.notes',
+    defaultMessage: 'Notes',
+  },
+  optional: {
+    id: 'app.components.Note.optional',
+    defaultMessage: 'Optional',
+  },
+  instructions: {
+    id: 'app.components.Note.instructions',
+    defaultMessage: 'Any special instructions?',
+  },
+});
