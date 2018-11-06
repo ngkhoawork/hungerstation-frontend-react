4ee0eec14e72bc71ede3d3481a583398e62b21db
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-06 13:15:11 +0900

    insufficient order amount hoc

---
+++ app/containers/InsufficientOrderAmount/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import { withRouter } from 'react-router-dom';
+import InsufficientOrderAmount from 'components/InsufficientOrderAmount';
+import { hideModal } from 'containers/ModalContainer/actions';
+
+const InsufficientOrderAmountHOC = ({ hideModal, history, location }) => {
+  const handleAddMore = () => {
+    // TODO: once Login page is fixed to not push but replace state this
+    // can be just: history.goBack();
+    const path = location.pathname.split('/');
+    path.pop(); // remove /checkout from pathname
+    history.push(path.join('/'));
+    hideModal();
+  };
+
+  return (
+    <InsufficientOrderAmount onCancel={hideModal} onAddMore={handleAddMore} />
+  );
+};
+
+InsufficientOrderAmountHOC.propTypes = {
+  history: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  hideModal: PropTypes.func.isRequired,
+};
+
+export default compose(
+  withRouter,
+  connect(
+    null,
+    { hideModal },
+  ),
+)(InsufficientOrderAmountHOC);
