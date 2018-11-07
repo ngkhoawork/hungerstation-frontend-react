ecb53d4320556297137a69bde080c028221808ae
jheros <jherosk@gmail.com>
2018-11-07 17:40:16 +0900

    CW-209 update restuarant card cover image

--- app/pages/RestaurantsPage/RestaurantsSection/RestaurantsList/RestaurantCard/Styled.js
+++ app/pages/RestaurantsPage/RestaurantsSection/RestaurantsList/RestaurantCard/Styled.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { flex, mediaLess } from 'utils/css/styles';
-import Figs from 'images/figs.png';
-import FigsMobile from 'images/figs-mobile.png';
+import BgPattern from 'images/bg-pattern.png';
+import BgPatternMobile from 'images/bg-pattern-mobile.png';
+import { gold } from 'utils/css/colors';
 
 const StyledRestaurantCard = styled(Link)`
   ${flex({
@@ -16,8 +17,8 @@ const StyledRestaurantCard = styled(Link)`
   border-radius: 8px;
   margin-bottom: 24px;
   margin-right: 24px;
-  padding: 15px 15px 5px;
-  background-image: ${({ cover }) => cover || `url(${Figs})`};
+  background-image: ${({ cover }) => cover || `url(${BgPattern})`};
+  background-color: ${gold};
   background-size: 140%;
   background-position: top;
   box-shadow: 0 10px 20px 0 rgba(126, 125, 125, 0.15);
@@ -30,7 +31,8 @@ const StyledRestaurantCard = styled(Link)`
 
   ${mediaLess(500)`
     min-width: 324px;
-    background-image: ${({ cover }) => cover || `url(${FigsMobile})`};
+    background-image: ${({ cover }) => cover || `url(${BgPatternMobile})`};
+    background-color: ${gold};
     margin-right: 0px;
     background-size: 100%;
   `};
@@ -40,11 +42,14 @@ const StyledUpperPart = styled.div`
   ${flex({ align: 'center', justify: 'space-between' })};
   width: 100%;
   flex: 0.4;
+  padding: 15px 15px 5px 15px;
 `;
 
 const StyledBottomPart = styled.div`
   ${flex({ align: 'flex-start', direction: 'column' })};
+  background-color: #fff;
   width: 100%;
+  padding: 5px 15px 15px 15px;
   flex: 0.4;
 
   div > p {
