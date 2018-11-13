dca84585f8632bddd3e8d06adbb0f4772bfeba60
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-13 21:01:02 +0900

    restaurant info updates

--- app/components/RestaurantInfo/index.js
+++ app/components/RestaurantInfo/index.js
@@ -57,59 +57,59 @@ const renderStatus = (status, { start_minute, end_minute } = {}) => {
   return null;
 };
 
-const RestaurantInfo = ({
-  logo,
-  name,
-  status,
-  working_times,
-  rateAverage,
-  cuisines,
-}) => (
-  <StyledRestaurantInfo>
-    <StyledLogo>
-      <BrandLogo src={logo} size={45} />
-    </StyledLogo>
-    <StyledDetailsContainer flex={0.7} justify="flex-start">
-      <StyledMobileLogo>
-        <BrandLogo src={logo} size={45} />
-      </StyledMobileLogo>
-      <StyledDetails>
-        <Name>
-          {name}
-          <RatingContainer>
-            <Icon size={10} name="star" />
-            {rateAverage}
-          </RatingContainer>
-        </Name>
-        <Group>
-          {cuisines.map(({ id, name, image }) => (
-            <StyledCuisine key={id}>
-              <img src={image} alt={name} height="14" />
-              <Paragraph size={12} color={alto}>
-                {name}
-              </Paragraph>
-            </StyledCuisine>
-          ))}
-        </Group>
-      </StyledDetails>
-    </StyledDetailsContainer>
-    <StatusContainer>{renderStatus(status, working_times)}</StatusContainer>
-  </StyledRestaurantInfo>
-);
+const RestaurantInfo = ({ restaurant, status, working_time }) => {
+  const workingTime = ((working_time || restaurant.working_time || {})
+    .weektimes || [])[0];
+
+  return (
+    <StyledRestaurantInfo>
+      <StyledLogo>
+        <BrandLogo src={restaurant.logo} size={45} />
+      </StyledLogo>
+      <StyledDetailsContainer flex={0.7} justify="flex-start">
+        <StyledMobileLogo>
+          <BrandLogo src={restaurant.logo} size={45} />
+        </StyledMobileLogo>
+        <StyledDetails>
+          <Name>
+            {restaurant.name}
+            <RatingContainer>
+              <Icon size={10} name="star" />
+              {restaurant.rate_average}
+            </RatingContainer>
+          </Name>
+          <Group>
+            {restaurant.kitchens.map(({ id, name, image_thumb }) => (
+              <StyledCuisine key={id}>
+                <img src={image_thumb} alt={name} height="14" />
+                <Paragraph size={12} color={alto}>
+                  {name}
+                </Paragraph>
+              </StyledCuisine>
+            ))}
+          </Group>
+        </StyledDetails>
+      </StyledDetailsContainer>
+      <StatusContainer>{renderStatus(status, workingTime)}</StatusContainer>
+    </StyledRestaurantInfo>
+  );
+};
 
 RestaurantInfo.propTypes = {
-  logo: PropTypes.string,
-  name: PropTypes.string.isRequired,
   status: PropTypes.string,
-  working_times: PropTypes.object,
-  rateAverage: PropTypes.number,
-  cuisines: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      image: PropTypes.string,
-    }),
-  ),
+  working_time: PropTypes.object,
+  restaurant: PropTypes.shape({
+    logo: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    rate_average: PropTypes.number,
+    kitchens: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        image_thumb: PropTypes.string,
+      }),
+    ),
+  }),
 };
 
 export default RestaurantInfo;
