ba1cacd5ed9321f0253c7e63f766e036029c5a54
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-13 20:58:53 +0900

    brand logo update

--- app/components/BrandLogo/index.js
+++ app/components/BrandLogo/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import intl from 'utils/intlService';
-import messages from './messages';
 import StyledLogo from './StyledLogo';
 
 const BrandLogo = ({ src, size = 32, isWithMargin }) => (
-  <StyledLogo
-    src={src}
-    alt={intl.formatMessage(messages.altText)}
-    size={size}
-    isWithMargin={isWithMargin}
-  />
+  <StyledLogo src={src} alt="" size={size} isWithMargin={isWithMargin} />
 );
 
 BrandLogo.propTypes = {
