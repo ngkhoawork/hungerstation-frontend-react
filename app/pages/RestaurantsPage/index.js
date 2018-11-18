55802450e24bfa1bcb9e119a463c25ebd71c688b
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-18 18:45:32 +0900

    code cleanup

--- app/pages/RestaurantsPage/index.js
+++ app/pages/RestaurantsPage/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import PageContent from 'components/PageContent';
 import Back from 'containers/Back';
-import styled from 'styled-components';
-import { flex } from 'utils/css/styles';
-
 import { withHeaderAndFooter } from 'hocs/withInsertLayout';
 import withRestaurants from 'hocs/withRestaurants';
-
 import Breadcrumbs from 'components/Breadcrumbs';
+import { NavHeader } from 'utils/css/styledComponents';
 import RestaurantsSection from './RestaurantsSection';
 import FiltersSection from './FiltersSection';
 import FiltersWrapper from './FiltersWrapper';
-
 import StyledListPage from './StyledListPage';
-
-const NavHeader = styled.div`
-  height: 56px;
-  padding: 0;
-  ${flex({ align: 'center', justify: 'flex-start' })};
-  margin-bottom: 20px;
-`;
 
 const RestaurantsPage = () => (
   <PageContent>
