431aac45c5c0f824b5aa50f5b75499c724ba3e37
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-18 20:58:10 +0900

    added delivery info to restaurant info

--- app/components/RestaurantInfo/index.js
+++ app/components/RestaurantInfo/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { alto } from 'utils/css/colors';
+import { getDeepProp } from 'utils/helpers';
 import BrandLogo from 'components/BrandLogo';
 import Paragraph from 'components/Paragraph';
 import Group from 'components/Group';
 import Icon from 'components/Icon';
+import DeliveryInfo from 'components/DeliveryInfo';
 import Status from './Status';
 import {
   Name,
@@ -44,6 +46,10 @@ const RestaurantInfo = ({ restaurant, ...props }) => (
             </StyledCuisine>
           ))}
         </Group>
+        <DeliveryInfo
+          {...getDeepProp(restaurant, ['delivery_conditions', 0])}
+          style={{ margin: '10px 0' }}
+        />
       </StyledDetails>
     </StyledDetailsContainer>
     <Status {...props} restaurant={restaurant} />
