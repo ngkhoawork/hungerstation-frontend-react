561412f05d428f50b40e6dd272a849d4526a719a
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-19 15:12:02 +0900

    made currency able to be of smaller size than price in order to follow designs

--- app/components/Price/index.js
+++ app/components/Price/index.js
@@ -14,20 +14,55 @@ const Price = styled.span`
   ${({ css }) => css};
 `;
 
-const OrderElement = ({ price, hasTag, ...props }) => (
-  <Price {...props}>
-    {hasTag ? (
-      <Fragment>
-        <Icon name="price-tag" offsetY="1" /> &nbsp;
-      </Fragment>
-    ) : null}
-    {intl.formatNumber(price, priceIntlOptions)}
-  </Price>
-);
+const OrderElement = ({ price, hasTag, currencyStyle, ...props }) => {
+  const getStyledPrice = () => {
+    const priceString = intl.formatNumber(price, priceIntlOptions);
+
+    if (!currencyStyle || typeof priceString !== 'string') return priceString;
+
+    const priceParts = priceString.replace(/^(\D+)/, '$1 ').split(' ');
+    const pt1 = parseFloat(priceParts[0]);
+    const pt2 = parseFloat(priceParts[1]);
+
+    if (Number.isNaN(pt1) && typeof pt2 === 'number') {
+      return (
+        <span>
+          <span style={currencyStyle}>{priceParts[0].trim()}</span>
+          &nbsp;
+          {priceParts[1].trim()}
+        </span>
+      );
+    }
+
+    if (typeof pt1 === 'number' && Number.isNaN(pt2)) {
+      return (
+        <span>
+          {priceParts[0].trim()}
+          &nbsp;
+          <span style={currencyStyle}>{priceParts[1].trim()}</span>
+        </span>
+      );
+    }
+
+    return priceString;
+  };
+
+  return (
+    <Price {...props}>
+      {hasTag ? (
+        <Fragment>
+          <Icon name="price-tag" /> &nbsp;
+        </Fragment>
+      ) : null}
+      {getStyledPrice()}
+    </Price>
+  );
+};
 
 OrderElement.propTypes = {
   price: PropTypes.number.isRequired,
   size: PropTypes.number,
+  currencyStyle: PropTypes.object,
   isPrimary: PropTypes.bool,
   hasTag: PropTypes.bool,
 };
