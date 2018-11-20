0f4c95551c2bc0097bcf0551032627af40401fcb
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-21 00:16:59 +0900

    checkbox icon disabled state added

--- app/components/CheckboxIcon/index.js
+++ app/components/CheckboxIcon/index.js
@@ -4,31 +4,32 @@ import styled from 'styled-components';
 import Icon from 'components/Icon';
 import { lightGray } from 'utils/css/colors';
 
-const CheckboxIcon = ({ isChecked, onClick }) => {
+const CheckboxEmpty = styled.div`
+  border: solid 1px ${lightGray};
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  cursor: ${({ isDisabled }) => (isDisabled ? 'default' : 'pointer')};
+`;
+
+const CheckboxIcon = ({ isChecked, onClick, isDisabled }) => {
+  const iconStyle = {
+    cursor: isDisabled ? 'default' : 'pointer',
+  };
+
   if (isChecked) {
     return (
       <Icon name="toggle-green" style={iconStyle} size={20} onClick={onClick} />
     );
   }
 
-  return <CheckboxEmpty onClick={onClick} />;
+  return <CheckboxEmpty onClick={onClick} isDisabled={isDisabled} />;
 };
 
 CheckboxIcon.propTypes = {
+  isDisabled: PropTypes.bool,
   isChecked: PropTypes.bool,
   onClick: PropTypes.func,
 };
 
 export default CheckboxIcon;
-
-const CheckboxEmpty = styled.div`
-  border: solid 1px ${lightGray};
-  border-radius: 50%;
-  width: 20px;
-  height: 20px;
-  cursor: pointer;
-`;
-
-const iconStyle = {
-  cursor: 'pointer',
-};
