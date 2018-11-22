0709212303f143a9a7961df284e6423002836feb
Nurazem Kaldybaev <nurazemkg@gmail.com>
2018-11-22 19:11:10 +0600

    CW-319 Copy Order Id to clipboard on click

--- app/pages/OrdersPage/OrderCard/OrderId.js
+++ app/pages/OrdersPage/OrderCard/OrderId.js
@@ -3,13 +3,16 @@ import { string, number } from 'prop-types';
 import Row from 'components/Row';
 import Paragraph from 'components/Paragraph';
 import { silverChalice } from 'utils/css/colors';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const OrderId = ({ id, textSize = 14 }) => (
   <Row align="center">
     <Paragraph size={textSize} color={silverChalice} margin="0 10px 0 0">
       ID
     </Paragraph>
-    <Paragraph size={textSize}>#{id}</Paragraph>
+    <CopyToClipboard text={id}>
+      <Paragraph size={textSize}>#{id}</Paragraph>
+    </CopyToClipboard>
   </Row>
 );
 
