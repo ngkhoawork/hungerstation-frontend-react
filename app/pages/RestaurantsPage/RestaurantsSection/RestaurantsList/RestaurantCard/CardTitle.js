5344bb94df90b88c5ffaaf87dc3942126a16c739
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-22 17:15:32 +0900

    removed wrongly nested html

--- app/pages/RestaurantsPage/RestaurantsSection/RestaurantsList/RestaurantCard/CardTitle.js
+++ app/pages/RestaurantsPage/RestaurantsSection/RestaurantsList/RestaurantCard/CardTitle.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { string } from 'prop-types';
 import styled from 'styled-components';
 import Row from 'components/Row';
-import Paragraph from 'components/Paragraph';
 import CircledItem from 'components/CircledItem';
-import { silverChalice, gold, persimmon } from 'utils/css/colors';
+import { fuscousGray, silverChalice, gold, persimmon } from 'utils/css/colors';
 import { sideMargin } from 'utils/css/styles';
 
 const mappedStatusToColor = {
@@ -18,6 +17,8 @@ const Title = styled.p`
   ${sideMargin('start', '6px')};
   margin-top: 0;
   margin-bottom: 0;
+  font-size: 17px;
+  color: ${fuscousGray};
 `;
 
 const CardTitle = ({ name, status }) => (
@@ -26,9 +27,7 @@ const CardTitle = ({ name, status }) => (
       color={mappedStatusToColor[status] || silverChalice}
       width={7}
     />
-    <Paragraph size={17}>
-      <Title>{name}</Title>
-    </Paragraph>
+    <Title>{name}</Title>
   </Row>
 );
 
