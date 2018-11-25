67def57968e1793913c06e3e6f112b456ce08a89
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-25 14:03:17 +0900

    insufficient order amount style updates

--- app/components/InsufficientOrderAmount/StyledComponents.js
+++ app/components/InsufficientOrderAmount/StyledComponents.js
@@ -1,28 +1,41 @@
-import styled from 'styled-components';
-import { flex, mediaMedium } from 'utils/css/styles';
+import styled, { css } from 'styled-components';
+import { flex, mediaMedium, mediaMediumGreater } from 'utils/css/styles';
 import { jade } from 'utils/css/colors';
+
+export const containerStyle = css`
+  padding: 0 20px 25px;
+  ${mediaMediumGreater`width: 424px;`};
+`;
 
 export const Content = styled.div`
   margin: 20px auto;
-  width: 280px;
   text-align: center;
 `;
 
 export const Footer = styled.div`
-  ${flex({ justify: 'center', wrap: 'wrap' })};
+  ${flex()};
 
   button {
-    margin: 5px 10px 10px;
+    flex-grow: 1;
   }
+
+  ${mediaMediumGreater`
+    button:first-child {
+      margin-right: 20px;
+    }
+  `};
 
   ${mediaMedium`
     ${flex({ direction: 'column' }, false)};
     align-items: center;
-    width: fit-content;
-    margin: 0 auto;
+    margin: 0 10%;
 
     button {
       width: 100%;
+
+      &:first-child {
+        margin-bottom: 20px;
+      }
     }
   `};
 `;
