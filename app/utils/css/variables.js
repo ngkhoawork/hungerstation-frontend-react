126d54fbe2d5ddeb1511d330ed1740bcec2a655e
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-27 14:49:15 +0900

    removed hardcoded oversized page width values

--- app/utils/css/variables.js
+++ app/utils/css/variables.js
@@ -9,6 +9,7 @@ export const boxShadow = '1px 5px 27px -3px rgba(59, 59, 59, 0.13)';
 export const borderRadius = '8px';
 export const border = `solid 1px ${lightGray}`;
 
+export const maxPageWidth = '1366px';
 export const pageMaxWidth = '1600px';
 export const pageMinWidth = '1366px';
 export const pageContentWidth = '1244px';
@@ -17,7 +18,6 @@ export const navHeaderHeight = '55px';
 export const maxModalHeight = '98vh';
 export const maxModalContentHeight = 'calc(98vh - 94px)';
 export const maxModalWidth = '98vw';
-export const maxPageWidth = '1446px';
 export const pageOffsetX = '113px';
 export const mobPageOffsetX = '30px';
 export const smallMobPageOffsetX = '20px';
