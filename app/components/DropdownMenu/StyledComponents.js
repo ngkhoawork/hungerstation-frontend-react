fe5d08244f1914bffd9ce8977b26e2f45770823b
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-11-29 23:27:58 +0900

    dropdown menu enhanced ability

--- app/components/DropdownMenu/StyledComponents.js
+++ app/components/DropdownMenu/StyledComponents.js
@@ -48,7 +48,7 @@ export const StyledSubMenu = styled.div`
   overflow-y: auto;
   position: absolute;
   top: ${dropdownHeight};
-  z-index: ${({ isVisible }) => (isVisible ? zIndexDropdown : -100)};
+  z-index: ${zIndexDropdown};
   border-radius: ${borderRadius};
   ${flex({ direction: 'column' })};
   white-space: nowrap;
