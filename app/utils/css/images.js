c3109724635ac1db7d954e52a44d0198b3d0ee51
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-12-03 13:30:25 +0900

    unused images cleanup

--- app/utils/css/images.js
+++ app/utils/css/images.js
@@ -1,6 +1,3 @@
-import Burger from 'images/burger.png';
-import BurgerMedium from 'images/burger-medium.png';
-import BurgerSmall from 'images/burger-small.png';
 import Pizza from 'images/pizza.png';
 import Networking from 'images/networking.png';
 import Googleplay from 'images/google-play.png';
@@ -12,12 +9,6 @@ import aboutUs from 'images/aboutUs.png';
 
 const getImage = name => {
   switch (name) {
-    case 'burger':
-      return Burger;
-    case 'burger-medium':
-      return BurgerMedium;
-    case 'burger-small':
-      return BurgerSmall;
     case 'pizza':
       return Pizza;
     case 'networking':
