b352b0170fe55dc409c2b44bf77097edb3b3f86d
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-12-04 19:21:34 +0900

    home page mobile apps section images updates and refactored

--- app/pages/HomePage/BottomSection/MobileAppSection/ButtonGroup.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { flex } from 'utils/css/styles';
-
-const ButtonGroup = styled.div`
-  ${flex({ align: 'center' })};
-`;
-
-export default ButtonGroup;
--- app/pages/HomePage/BottomSection/MobileAppSection/index.js
+++ app/pages/HomePage/BottomSection/MobileAppSection/index.js
@@ -1,56 +1,23 @@
 import React from 'react';
 import intl from 'utils/intlService';
-import styled, { css } from 'styled-components';
 import Icon from 'components/Icon';
 import Button from 'components/Button';
-import { mediaMedium, sideMargin, sidePosition } from 'utils/css/styles';
-import { wildSand } from 'utils/css/colors';
-import PhoneVisualSmall from 'images/phone-visual-small.png';
-import PhoneVisual from 'images/phone-visual.png';
 import Section from '../Section';
 import SectionDetails from '../SectionDetails';
-import ButtonWrapper from './ButtonWrapper';
-import ButtonGroup from './ButtonGroup';
-import DetailsWrapper from './DetailsWrapper';
-
+import {
+  DetailsWrapper,
+  ButtonWrapper,
+  ButtonGroup,
+  MobileBackgroundContainer,
+  MobileBackground,
+  rightSectionCss,
+  sectionCss,
+} from './StyledComponents';
 import messages from './messages';
-
-const MobileBackgroundContainer = styled.div`
-  background-image: url(${PhoneVisual});
-  background-size: 100%;
-  background-position: right bottom;
-  ${sideMargin('end', '-90px')};
-  overflow: visible;
-  position: relative;
-  padding-top: 100%;
-  background-color: ${wildSand};
-
-  ${mediaMedium`
-    background-image: none;
-    ${sideMargin('end', '0')}
-  `};
-`;
-
-const MobileBackground = styled.div`
-  ${mediaMedium`
-    background-image: url(${PhoneVisualSmall});
-    position: absolute;
-    padding-top: 126%;
-    width: 100%;
-    bottom: -15%;
-    ${sidePosition('start', '0')};
-    background-size: cover;
-    
-  `};
-`;
-
-const rightSectionCss = css`
-  ${mediaMedium`
-  `};
-`;
 
 const MobileAppSection = () => (
   <Section
+    css={sectionCss}
     hasBackground
     leftSection={
       <DetailsWrapper>
