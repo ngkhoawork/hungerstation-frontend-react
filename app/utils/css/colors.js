b72af3d2caab7214119809b792cd3e55778911ef
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-12-05 22:01:45 +0900

    added style variables

--- app/utils/css/colors.js
+++ app/utils/css/colors.js
@@ -13,6 +13,7 @@ export const successBg = '#f3fbf5';
 export const boulder = '#7C7C7C';
 export const dustyGray = '#979797';
 export const silverChalice = '#9F9F9F';
+export const gray = '#B4B3AD';
 export const lightGray = '#f0f0f0';
 export const lighterGray = '#f2f2f2';
 export const lightestGray = '#fafafa';
