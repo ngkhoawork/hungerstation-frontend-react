17a9bab219a560fbeb68231ceb0c8f74f0041111
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-12-05 21:57:59 +0900

    group taking style props

--- app/components/Group/index.js
+++ app/components/Group/index.js
@@ -1,8 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { flex } from 'utils/css/styles';
 
 const Group = styled.div`
   ${flex({ align: 'center', justify: 'flex-start' })};
+
+  ${({ style }) => style && css(style)};
 `;
 
 export default Group;
