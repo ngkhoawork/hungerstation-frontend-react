c36f77912387142115e96940a2f93ae6a9edce65
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-12-27 21:29:10 +0900

    removed clearing location reducer on logout

--- app/modules/location/reducer.js
+++ app/modules/location/reducer.js
@@ -1,6 +1,5 @@
 import { fromJS } from 'immutable';
 import { sortAlphabetically } from 'utils/helpers';
-import { logout } from 'modules/auth/actions';
 import {
   setCitiesAction,
   setDistrictsAction,
@@ -41,8 +40,6 @@ function searchBarContainerReducer(state = initialState, action) {
       return onSaveCurrentLocationAction(state, action);
     case saveLocation.type:
       return onSaveLocation(state, action);
-    case logout.type:
-      return initialState;
     default:
       return state;
   }
